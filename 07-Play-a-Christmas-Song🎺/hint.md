844660e178bfb443c998efc1a8feb1ad4a4efb96
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-08 01:36:24 +0600

    🎺 added challenge 7

---
+++ 07-Play-a-Christmas-Song🎺/hint.md
@@ -0,0 +1,6 @@
+- Check out the audio play() method
+
+- To stop the audio, consider using this:
+
+sound.pause();
+sound.currentTime = 0;
---
+++ 07-Play-a-Christmas-Song🎺/index.js
@@ -0,0 +1,10 @@
+const playBtn = document.getElementById("play-btn")
+const pauseBtn = document.getElementById("pause-btn")
+const stopBtn = document.getElementById("stop-btn")
+
+// Task:
+// - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
+
+// Stretch goals:
+// - Add volume controls.
+// - Allow the user to select different sounds.
