267af157e1598b6ad60abe52ef88350fa951bd53
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-15 23:05:44 +0600

    🧝 added challenge 14

---
+++ 14-Lonely-Elf 🧝/hint.md
@@ -0,0 +1,3 @@
+- use textContent to duplicate the elf
+- check out the CSS ch unit
+- check out the CSS overflow-y property
---
+++ 14-Lonely-Elf 🧝/index.js
@@ -0,0 +1,14 @@
+const elf = document.getElementById("elf")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", duplicateElf)
+
+function duplicateElf(){
+    // Task:
+   // - Write a function to duplicate the elf when the button is clicked.
+   // - See index.css for optional styling challenges.
+}
+
+// Stretch goals:
+// - Write a function to give the elves some tools, or a cup of tea!
+// - Limit the total number of elves to 100.
