004cc33581f218dc798467e7f0b2061156a6e3e8
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-15 23:02:18 +0600

    🦃 added challenge 13

---
+++ 13-Christmas-Dinner-Calulator 🦃/hint.md
@@ -0,0 +1,2 @@
+- Use a conditional to decide the dinner.
+- Render with text.Content.
---
+++ 13-Christmas-Dinner-Calulator 🦃/index.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("btn")
+let food = document.getElementById("food")
+
+// Tasks:
+// - Write the JS to decide the perfect Christmas dinner and render it in the result element. Don't forget to check whether the meal should be vegetarian!
+
+// Dinner suggestions (or choose your own!):
+// Vegetarian: nut roast
+// 4 people or less: turkey
+// 5+ people: goose 
+
+// Stretch goals:
+// - Add more dietary options.
+// - Show recipes when the meal has been selected.
+
