20542b2b8273c8ab2138ac7a814412094161ac74
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-19 21:53:11 +0600

    📺 added challenge 16

---
+++ 16-Christmas-Movie-Selector 📺/hint.md
@@ -0,0 +1,1 @@
+Grab the values from the <option> tags and then loop through moviesArr to find a match.
---
+++ 16-Christmas-Movie-Selector 📺/index.js
@@ -0,0 +1,36 @@
+const ageSelector = document.getElementById("age-selector")
+const genreSelector = document.getElementById("genre-selector")
+
+const btn = document.getElementById("btn")
+const suggestedMovie = document.getElementById("suggested-movie")
+
+const moviesArr = [
+    {
+        name: "Die Hard",
+        age: "18+",
+        genre: "Action"
+    },
+    {
+        name: "Love Actually",
+        age: "18+",
+        genre: "Romance"
+    },
+    {
+        name: "The Polar Express",
+        age: "PG",
+        genre: "Action"
+    },
+    {
+        name: "Shrek",
+        age: "PG",
+        genre: "Romance"
+    }
+]
+
+// Task: 
+// - Write a function to select a suitable movie based on the age group and genre provided.
+//  - Display it in the suggested-movie paragraph when the button is clicked.
+
+// Stretch goals: 
+// - Have the function run on each change of the <select> tags.
+// - Add more movies/complexity - for example black and white vs color, preferred actors, etc. 
