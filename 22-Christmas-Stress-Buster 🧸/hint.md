9e278cf1e47bc4fa9f240993c4e72a168fdf97c9
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-25 23:51:42 +0600

    🧸 added challenge 22

---
+++ 22-Christmas-Stress-Buster 🧸/hint.md
@@ -0,0 +1,1 @@
+Check whether the time left is 0. If not, decrement one second from the time.
---
+++ 22-Christmas-Stress-Buster 🧸/index.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("btn")
+let timer = document.getElementById("timer")
+let affirmation = document.getElementById("affirmation")
+let timeout;
+
+// Task:
+// - Write a function to start the countdown timer on the button click and display it in the DOM. Stop the timer when it reaches 0.
+
+// Stretch goals:
+// - Show a relaxing image in the background while the timer is running.
+// - Play relaxing music.
