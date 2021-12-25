757e2bb2d3364050d5dfc9ceab8f31e23d7dd525
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-25 23:53:54 +0600

    🎅 added last challenge 24

---
+++ 24-Sleigh-Launcher 🎅/hint.md
@@ -0,0 +1,1 @@
+Create an animation in the CSS file, use classList.add to add it to the crew.
---
+++ 24-Sleigh-Launcher 🎅/index.js
@@ -0,0 +1,14 @@
+const crew = document.getElementById("crew")
+document.getElementById("btn").addEventListener("click", launch)
+
+function launch() {
+    crew.classList.add("bounce-out-top")
+}
+
+// Task:
+// - Write a function to launch the sleigh! 
+// - See CSS for more tasks.
+
+// Stretch goals:
+// - Add a cheering sound when the sleigh takes off.
+// - Add a countdown to the launch time.
